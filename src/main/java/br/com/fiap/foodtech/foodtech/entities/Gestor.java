30d0fe4b2ccb8549d9acbfa4ce688640f6a685e0
elias pereira <eliaspinheiropereiraa@gmail.com>
2025-05-26 17:13:00 -0300

    ajustando o mapeamento do jpa para criar uma tabela para cada usuario

--- src/main/java/br/com/fiap/foodtech/foodtech/entities/Gestor.java
+++ src/main/java/br/com/fiap/foodtech/foodtech/entities/Gestor.java
@@ -1,17 +1,23 @@
 package br.com.fiap.foodtech.foodtech.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Entity
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "gestor")
 public class Gestor extends Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     public Gestor(String nome, String email, String login, String senha) {
         super(nome, email, login, senha);
