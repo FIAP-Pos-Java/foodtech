a4ba42cc225b7d95e0489d3cd8b104cdd62766c4
Henrique da Gama Saczkowski <henrique1443@hotmail.com>
2025-07-31 14:48:49 -0300

    Removidos endpoints, entidades e casos de uso relacionados à gestão de clientes e gestores, incluindo configurações, validações, exceções e documentação.

--- src/main/java/br/com/fiap/foodtech/foodtech/service/exceptions/ResourceAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.foodtech.foodtech.service.exceptions;
-
-public class ResourceAlreadyExistsException extends RuntimeException {
-
-    public ResourceAlreadyExistsException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/br/com/fiap/foodtech/foodtech/service/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.foodtech.foodtech.service.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
-}
